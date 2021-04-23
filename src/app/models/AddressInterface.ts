b60354c2abf377c9f169cf446fb8a4b169fb8ed8
webcurso12345 <webcurso12345@gmail.com>
2021-04-23 20:47:10 -0300

    Make lat, lon and line2 properties optional on AddressInterface

--- src/app/models/AddressInterface.ts
+++ src/app/models/AddressInterface.ts
@@ -1,10 +1,10 @@
 interface AddressInterface {
   city: string;
   country: string;
-  lat: number;
-  lon: number;
+  lat?: number;
+  lon?: number;
   line1: string;
-  line2: string;
+  line2?: string;
   state: string;
   zipcode: string;
 }
